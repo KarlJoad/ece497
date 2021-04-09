a6522d083507e763be3306770b9d461e403cddd3
Karl Hallsby <karl@hallsby.com>
2021-04-09 11:55:23 -0500

    Include script to fully compile the manual

---
+++ tutorials/compile-doc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+pdflatex --synctex=1 -interaction=nonstopmode -shell-escape doc.tex
+biber doc
+makeglossaries doc
+# makeindex -s "doc.ist" -t "doc.alg" -o "doc.acr" "doc.acn"
+
+pdflatex --synctex=1 -interaction=nonstopmode -shell-escape doc.tex
+pdflatex --synctex=1 -interaction=nonstopmode -shell-escape doc.tex
+pdflatex --synctex=1 -interaction=nonstopmode -shell-escape doc.tex
