03b6bf5b772cb479069959b91efb0f3061cfd242
Karl Hallsby <karl@hallsby.com>
2021-04-12 19:10:13 -0500

    Update compile script to ensure proper compilation with reference in glossary
    Update compile script

--- tutorials/compile-doc.sh
+++ tutorials/compile-doc.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 pdflatex --synctex=1 -interaction=nonstopmode -shell-escape doc.tex
+
 biber doc
+
 makeglossaries doc
+biber doc
 makeglossaries doc
 # makeindex -s "doc.ist" -t "doc.alg" -o "doc.acr" "doc.acn"
 
 pdflatex --synctex=1 -interaction=nonstopmode -shell-escape doc.tex
+biber doc
+
 pdflatex --synctex=1 -interaction=nonstopmode -shell-escape doc.tex
 pdflatex --synctex=1 -interaction=nonstopmode -shell-escape doc.tex
