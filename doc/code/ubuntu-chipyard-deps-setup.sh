91e49e990e4f008ae5d83093076240a51a220d17
Karl Hallsby <karl@hallsby.com>
2021-04-28 00:41:40 -0500

    Update ubuntu dependency script for paper

--- doc/code/ubuntu-chipyard-deps-setup.sh
+++ doc/code/ubuntu-chipyard-deps-setup.sh
@@ -2,23 +2,21 @@
 
 set -ex
 
+sudo apt-get install -y curl
 sudo apt-get install -y build-essential bison flex
 sudo apt-get install -y libgmp-dev libmpfr-dev libmpc-dev zlib1g-dev vim git default-jdk default-jre
-# install sbt: https://www.scala-sbt.org/release/docs/Installing-sbt-on-Linux.html
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+# install sbt: https://www.scala-sbt.org/release/docs/Installing-sbt-on-Linux.html#Ubuntu+and+other+Debian-based+distributions
+echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
 sudo apt-get update
-# Scala Built Tool
 sudo apt-get install -y sbt
-# For the `info' command and parsing command line arguments
 sudo apt-get install -y texinfo gengetopt
-# Additional dependencies
 sudo apt-get install -y libexpat1-dev libusb-dev libncurses5-dev cmake
 # deps for poky
-sudo apt-get install -y python3.6 patch diffstat texi2html texinfo subversion chrpath git wget
+sudo apt-get install -y python3.8 patch diffstat texi2html texinfo subversion chrpath git wget
 # deps for qemu
 sudo apt-get install -y libgtk-3-dev gettext
 # deps for firemarshal
-sudo apt-get install -y python3-pip python3.6-dev rsync libguestfs-tools expat ctags
+sudo apt-get install -y python3-pip python3.8-dev rsync libguestfs-tools expat ctags
 # install DTC
 sudo apt-get install -y device-tree-compiler
